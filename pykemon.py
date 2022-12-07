b29e736931fe4a3e941ecf2fa97887315971852b
Entropie67 <olivier.elophe@gmail.com>
2022-12-07 10:39:34 +0100

    init

--- pykemon.py
+++ pykemon.py
@@ -1,11 +1,16 @@
 class Pokemon:
 
-    def __init__(self):
-        pass
+    def __init__(self, nom1, nom2, nom3, power, hp, exp=0):
+        self.stade = 0
+        self.noms = [nom1, nom2, nom3]
+        self.nom = self.noms[self.stade]
+        self.power = power
+        self.hp = hp
+        self.exp = exp
 
 
     def evolution(self):
-        pass
+        if self.stade >= 2:
+            print(f"{self.nom} ne peut plus évoluer. Sa puissance est au max")
 
 
-    
