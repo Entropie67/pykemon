a17fc1a81e7a11b3b5aad411b241bf910537401e
Entropie67 <olivier.elophe@gmail.com>
2022-12-07 11:05:14 +0100

    abra test

--- pykemon.py
+++ pykemon.py
@@ -1,16 +1,36 @@
 class Pokemon:
 
     def __init__(self, nom1, nom2, nom3, power, hp, exp=0):
-        self.stade = 0
-        self.noms = [nom1, nom2, nom3]
+        self.stade = (exp//1000) % 3
+        self.noms = [nom1, nom2, nom3, "Méga-"+nom3]
         self.nom = self.noms[self.stade]
         self.power = power
         self.hp = hp
         self.exp = exp
+        self.type = "plante"
 
 
-    def evolution(self):
+    def _evolution(self):
+
         if self.stade >= 2:
             print(f"{self.nom} ne peut plus évoluer. Sa puissance est au max")
+        else:
+            print(" \n *** Evolution *** \n")
+            print(f"Votre{self.nom} devient un {self.noms[self.stade]}")
+            self.nom = self.noms[self.stade]
 
+    def add_xp(self, exp):
+        self.exp += exp
+        if self.stade != (exp//1000) % 3:
+            self.stade = (exp//1000) % 3
+            self._evolution()
+
+
+ ############################
+ # TEST
+ ###########################
+
+abra = Pokemon("Abra", "Kadabra", "Alakazam", 1000, 100)
+
+print(abra.nom)
 
